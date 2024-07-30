370b508218ecd774a6204a58cfffdeda4960cbb0
Nitin Rajput <Ntnraj14@gmail.com>
2024-07-30 21:08:08 +0530

    navigation

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { useRouter } from 'next/navigation';
 
@@ -8,17 +8,33 @@ import EventCards from '../_components/event-cards/event-cards';
 
 import { IEvent } from '../_interfaces/event.interface';
 
+import { getEventDetails } from '../_lib/store/slices/event-details-slice';
+import { getEventList } from '../_lib/store/slices/events-slice';
+import { useSelector, useDispatch } from '../_lib/store/store';
+
 interface EventCardsProps {
     eventList: IEvent[] | null;
 }
 
 export default function Home(eventList : EventCardsProps) {
-    const router = useRouter()
+    const router = useRouter();
+    const dispatch = useDispatch();
+    // Select the 'cardDetails' data from the Redux store using useSelector
+    const { eventsList } = useSelector((state) => state.eventList);
+    if(eventList.eventList?.length === 0) {
+
+    } else if (!eventList.eventList?.length) {
+      eventList = {
+        eventList: eventsList
+      }
+    }
 
     const handleCardClick = (item: IEvent): void => {
-        console.log(item);
-        router.push('/event-details')        
+      dispatch(getEventList(eventList.eventList))
+      dispatch(getEventDetails(item));
+      router.push(`/event-details?`)
     }
+
 
   return (
     <>
